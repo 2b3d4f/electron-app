44c432db1ad92c125bae680bae1b5f1339b0753d
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-09-05 12:33:45 +0900

    update styles

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -5,30 +5,48 @@ function App(): React.JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
+    <div className="flex min-h-dvh select-none flex-col justify-center items-center bg-sky-50">
+      <img alt="logo" className="size-32" src={electronLogo} />
+      <div className="mt-2 text-sm bg-sky-400 text-sky-50 px-4 py-1 rounded-full">
+        Powered by electron-vite
       </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
+      <div className="mt-4 text-lg font-semibold text-sky-900">
+        Build an Electron app with React and TypeScript
+      </div>
+      <p className="mt-1 text-sky-900">
+        Please try pressing{' '}
+        <kbd
+          className="inline-block px-1 rounded-sm
+         font-mono font-semibold bg-blue-200"
+        >
+          F12
+        </kbd>{' '}
+        to open the devTool
       </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
+      <div className="flex gap-4 mt-4">
+        <div>
+          <a
+            href="https://electron-vite.org/"
+            target="_blank"
+            rel="noreferrer"
+            className="inline-block px-4 py-2 rounded-full bg-sky-900 text-sm text-sky-100 font-semibold hover:bg-sky-800 cursor-pointer"
+          >
             Documentation
           </a>
         </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+        <div>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            onClick={ipcHandle}
+            className="inline-block px-4 py-2 rounded-full bg-sky-900 text-sm text-sky-100 font-semibold hover:bg-sky-800 cursor-pointer"
+          >
             Send IPC
           </a>
         </div>
       </div>
       <Versions></Versions>
-    </>
+    </div>
   )
 }
 
--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -4,10 +4,10 @@ function Versions(): React.JSX.Element {
   const [versions] = useState(window.electron.process.versions)
 
   return (
-    <ul className="versions">
-      <li className="electron-version">Electron v{versions.electron}</li>
-      <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
+    <ul className="fixed bottom-4 inset-x-0 mx-auto w-max flex text-blue-900 bg-blue-200 font-mono font-bold rounded-full py-2 *:not-last:border-r *:border-blue-900 *:px-4">
+      <li>Electron v{versions.electron}</li>
+      <li>Chromium v{versions.chrome}</li>
+      <li>Node v{versions.node}</li>
     </ul>
   )
 }
