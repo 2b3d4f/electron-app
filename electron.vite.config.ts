225fbe79c0aef1f07bf6c6b84054bb413c8e3b71
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-09-07 13:57:34 +0900

    update config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   main: {
@@ -16,6 +17,15 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react(), tailwindcss()]
+    plugins: [
+      tailwindcss(),
+      tanstackRouter({
+        target: 'react',
+        autoCodeSplitting: true,
+        routesDirectory: 'src/renderer/src/routes',
+        generatedRouteTree: 'src/renderer/src/routeTree.gen.ts'
+      }),
+      react()
+    ]
   }
 })
